4aeae173fc4bffc2bff7b8bfc1b27db0a6188614
Becca Sheldon <rebecca.e.sheldon@gmail.com>
2013-11-01 13:37:21 -0500

    adds current user identification to the current user page

--- app/assets/javascripts/users/users.js
+++ app/assets/javascripts/users/users.js
@@ -1,7 +1,8 @@
 // use debugger to break code and see what's up
 // debugger = bff for figuring out js issues
 
+
+// this makes a drop down date picker calendar
 $(document).ready(function() {
 	$('input[name="appointment[time]"]').datepicker();
 });
-
